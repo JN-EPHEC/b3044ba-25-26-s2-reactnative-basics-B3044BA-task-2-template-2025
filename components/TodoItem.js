d59a886cdf2b07e8fa3697d8889e7aa06980857b
Timotéo Peeters <HE133074@students.ephec.be>
2025-10-05 13:19:46 +0200

    homework1

---
+++ components/TodoItem.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+
+export default function Todoitems ({text})
+{
+    return (
+        <View>
+            <Text>{text}</Text>
+        </View>
+    ) ; 
+}
