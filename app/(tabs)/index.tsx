15208eac2e7290af3e974feb29d8d7aa18f67468
SamiBnck <sami.bmt@hotmail.com>
2025-10-08 11:39:55 +0200

    nbfhf

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,30 +1,54 @@
-import TodoItem from "@/components/ui/TodoItem";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import ProfileCard from "@/components/ProfileCard";
+import { ScrollView, StyleSheet } from "react-native";
+
+const USERS_DATA = [
+  { id: '1', nom: 'Alice Martin', email: 'alice.martin@email.com' },
+  { id: '2', nom: 'Benjamin Dubois', email: 'ben.dubois@email.com' },
+  { id: '3', nom: 'Chloé Garcia', email: 'chloe.g@email.com' },
+  { id: '4', nom: 'David Petit', email: 'david.petit@email.com' },
+  { id: '5', nom: 'Émilie Rousseau', email: 'emilie.rousseau@email.com' },
+];
+
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>My Todo List</Text>
-
-      <TodoItem text="Learn React Native" />
-      <TodoItem text="Build a static UI" />
-      <TodoItem text="Practice with StyleSheet" />
-      <TodoItem text="Finish the Todo exercise" />
-    </View>
+      <ScrollView>
+        <ProfileCard
+  name="Samsko"
+  jobTitle="<Impoli>"
+  imageUrl="https://picsum.photos/200/300"
+/>
+      </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#f5f5f5",
-    paddingTop: 50,
+    backgroundColor: '#f5f5f5',
   },
   title: {
-    fontSize: 28,
-    fontWeight: "bold",
-    textAlign: "center",
-    marginBottom: 20,
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginVertical: 20,
+  },
+  item: {
+    backgroundColor: '#ffffff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: '#ddd',
+  },
+  nom: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  email: {
+    fontSize: 14,
+    color: '#666',
+    marginTop: 4,
   },
 });
