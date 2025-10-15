ba013d6a3c12f4da3e518084e7f4261d9230250d
samigard04 <sami.gard04@gmail.com>
2025-10-15 10:33:39 +0200

    devoir

--- app/(tabs)/counter.tsx
+++ app/(tabs)/counter.tsx
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import { SafeAreaView, StyleSheet, Text } from "react-native";
+import { useState } from "react";
 
 export default function HomeScreen() {
   // Initialize state for the counter with an initial value of 0.
@@ -7,31 +6,17 @@ export default function HomeScreen() {
 
   // Function to increment the count.
   const incrementCount = () => {
-    // Write your logic here to increment the count
+    setCount(count + 1);
   }
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>Replace this part with your soluce</Text>
-    </SafeAreaView>
-  );
-}
+  const decrementCount =() => {
+    setCount (count -1);
+  }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#f0f0f0",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 20,
-  },
-  countText: {
-    fontSize: 48,
-    fontWeight: "600",
-    marginVertical: 20,
-  },
-});
+  const Reset=() => {
+    setCount(0);
+  }
+
+  const increment100count = () => {
+    setCount(count +100);
+  }}
